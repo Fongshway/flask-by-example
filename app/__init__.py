7303139a89bce39cbb2e4d2c150ae15621ea47a9
Fongshway <kmf78@georgetown.edu>
2018-09-03 14:29:50 -0700

    Add user model and update init

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 __version__ = '0.1.0'
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
