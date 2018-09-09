92e6203664eca25a730d9442112087e71a50918b
Fongshway <kmf78@georgetown.edu>
2018-09-09 14:10:58 -0700

    Add require login and next redirect

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 from app import app
 from app.forms import LoginForm
@@ -8,6 +9,7 @@ from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': "Kevin"}
     posts = [
@@ -42,7 +44,14 @@ def login():
             flash('Invalid username or password. Please try again.')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+
+        # If the login URL does not have a next argument or includes a
+        # next argument that is set to a full URL that includes a domain
+        # name, then redirect the user to the index page.
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
