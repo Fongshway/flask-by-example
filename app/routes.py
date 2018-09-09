4369d134d41bd75daf48abf7a26311ee427fd05a
Fongshway <kmf78@georgetown.edu>
2018-09-09 11:51:20 -0700

    Add yapf script runner (#5)

--- app/routes.py
+++ app/routes.py
@@ -9,15 +9,19 @@ def index():
     user = {'username': "Kevin"}
     posts = [
         {
-            'author': {'username': 'John'},
+            'author': {
+                'username': 'John'
+            },
             'body': 'Beautiful day in Seattle!'
-        },
-        {
-            'author': {'username': 'Viola'},
+        }, {
+            'author': {
+                'username': 'Viola'
+            },
             'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'author': {'username': 'Test'},
+        }, {
+            'author': {
+                'username': 'Test'
+            },
             'body': 'This is a test'
         }
     ]
@@ -28,7 +32,6 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
