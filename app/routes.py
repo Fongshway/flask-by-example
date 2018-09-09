907bc624e8bab531df8b1da7e35aaa1cede0a6dc
Fongshway <kmf78@georgetown.edu>
2018-09-09 14:15:04 -0700

    Use user from db

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,6 @@ from app.models import User
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': "Kevin"}
     posts = [
         {
             'author': {
@@ -30,7 +29,7 @@ def index():
             'body': 'This is a test'
         }
     ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home", posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
