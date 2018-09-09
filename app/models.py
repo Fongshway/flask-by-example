45ff1c2b07b50804b4410a60caca70d5b95bd3f6
Fongshway <kmf78@georgetown.edu>
2018-09-09 14:57:41 -0700

    The Flask Mega-Tutorial Part V: User Logins (#6)
    https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-v-user-logins

--- app/models.py
+++ app/models.py
@@ -1,10 +1,18 @@
 from builtins import repr
 from datetime import datetime
 
-from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db, login
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -16,6 +24,12 @@ class User(db.Model):
 
     def __str__(self):
         return "{} - {}".format(self.username, self.email)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
