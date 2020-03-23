500e154fb4ea606a06c3df1aa80ecaec04c53368
Fongshway <8451964+Fongshway@users.noreply.github.com>
2020-03-22 19:06:26 -0700

    fixup

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Build Docker image
+set -e
 export IMAGE_TAG="fongshway/flask-by-example:develop"
 export REQUIREMENTS_INSTALL_CMD="${REQUIREMENTS_INSTALL_CMD:-pipenv install --dev --deploy --system}"
 
